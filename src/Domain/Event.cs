a24c1cc337b530284df8a000a6830fca1ca9fe04
Miguel Vazquez <miguelornelas54@gmail.com>
2026-09-07 04:05:41 -0600

    feat(parcial_1): add first entity and initial migration

--- src/Domain/Event.cs
+++ src/Domain/Event.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace Domain;
 
-public class Activity
+public class Event
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public required string Title { get; set; }
-    public  DateTime Date { get; set; }
+    public DateTime Date { get; set; }
     public required string Description { get; set; }
     public required string Category { get; set; }
     public bool IsCancelled { get; set; }
